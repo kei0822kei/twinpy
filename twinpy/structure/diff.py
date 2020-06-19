e0cd364a418e3472488432cb41cda7ca6a45c5db
kei0822kei <mizokami.keiyu@gmail.com>
2020-06-19 15:57:28 +0900

    add structure diff

---
+++ twinpy/structure/diff.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+compare structures
+"""
+
+import numpy as np
+
+def get_structure_diff(*cells):
+    """
+    get structure diff
+
+    Args:
+        cells: cell = (lattice, scaled_positions, symbols)
+
+    Returns:
+        dict: diff info
+
+    Note:
+        return diff compared with first cell
+    """
+    cart_posis = [ np.dot(cells[i][0].T, cells[i][1].T).T
+                       for i in range(len(cells)) ]
+    base_lat, base_scaled_posi, _ = cells[0]
+    base_cart_posi = cart_posis[0]
+    base_cart_posi = np.dot(base_lat.T, base_scaled_posi.T).T
+    lattice_diffs = [ cell[0] - base_lat for cell in cells ]
+    scaled_posi_diffs = [ cell[1] - base_scaled_posi for cell in cells ]
+    cart_posi_diffs = [ cart_posis[i] - base_cart_posi
+                            for i in range(len(cells)) ]
+    return {
+            'lattice_diffs': lattice_diffs,
+            'scaled_posi_diffs': scaled_posi_diffs,
+            'cart_posi_diffs': cart_posi_diffs,
+           }
