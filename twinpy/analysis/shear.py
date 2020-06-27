9d6627c287a846d420f845ba93f1fb9a9196cfa9
kei0822kei <mizokami.keiyu@gmail.com>
2020-06-27 15:38:21 +0900

    add band plot

--- twinpy/analysis/shear.py
+++ twinpy/analysis/shear.py
@@ -9,14 +9,15 @@ import numpy as np
 from twinpy.structure.base import (get_phonopy_structure,
                                    get_cell_from_phonopy_structure)
 from twinpy.structure.diff import get_structure_diff
+from twinpy.common.plot import (bands_plot,
+                                get_plot_properties_from_trajectory)
 
 class ShearAnalizer():
     """
     analize shear result
 
        .. attribute:: att1
-
-          Optional comment string.
+Optional comment string.
 
 
        .. attribute:: att2
@@ -46,45 +47,127 @@ class ShearAnalizer():
                                              structure_type=structure_type,
                                              symprec=symprec)
                        for cell in orig_cells ]
-        same_lattices = [ np.allclose(structure.get_cell(), input_cells[i][0])
-                          for i, structure in enumerate(structures) ]
-        if False in same_lattices:
+        are_same_lattices = [ np.allclose(structure.get_cell(), input_cells[i][0])
+                              for i, structure in enumerate(structures) ]
+        if False in are_same_lattices:
             raise RuntimeError("some lattices could not be detected relation "
                                "between original cell and input cell")
-        else:
-            self.structures = structures
-        self._original_structures = structures
-        self._relax_structures = None
+        self._original_cells = orig_cells
+        self._relax_input_structures = structures
+        self._relax_output_structures = None
+        self._phonons = None
 
-    def set_relax_structures(self, relax_cells:list):
+    def set_relax_output_structures(self, relax_cells:list):
         """
         Args:
             relax_cells (list): relax cells
         """
-        self._relax_structures = [ get_phonopy_structure(cell)
+        self._relax_output_structures = [ get_phonopy_structure(cell)
                                      for cell in relax_cells ]
 
+    def set_phonons(self, phonons):
+        """
+        set phonons
+        """
+        self._phonons = phonons
+
+    def get_phonons(self):
+        """
+        get phonons
+        """
+        return self._phonons
+
     @property
-    def original_structures(self):
+    def original_cells(self):
+        """
+        original cells
+        """
+        return self._original_cells
+
+    @property
+    def relax_input_structures(self):
         """
         original structures
         """
-        return self._original_structures
+        return self._relax_input_structures
 
     @property
-    def relax_structures(self):
+    def relax_output_structures(self):
         """
         relax structures
         """
-        return self._relax_structures
+        return self._relax_output_structures
 
     def get_relax_diffs(self):
         """
         get structure diffs between original and relax structures
         """
         diffs = [ get_structure_diff(get_cell_from_phonopy_structure(
-                                         self._original_structures[i]),
+                                         self._relax_input_structures[i]),
                                      get_cell_from_phonopy_structure(
-                                         self._relax_structures[i]))
-                  for i in range(len(self._original_structures)) ]
+                                         self._relax_output_structures[i]))
+                  for i in range(len(self._relax_input_structures)) ]
         return diffs
+
+    def get_shear_diffs(self):
+        """
+        get structure diffs between original and sheared structures
+
+        TODO:
+            rotation matrix does not considered, which is not correct
+        """
+        cells = [ get_cell_from_phonopy_structure(ph_structure)
+                    for ph_structure in self._relax_output_structures ]
+        diffs = get_structure_diff(*cells)
+        return diffs
+
+    def plot_bands(self,
+                   fig,
+                   with_dos=False,
+                   mesh=None,
+                   band_labels=None,
+                   segment_qpoints=None,
+                   is_auto=False,
+                   xscale=20,
+                   npoints=51,
+                   labels=None,):
+        """
+        plot phonon bands
+
+        Args:
+            arg1 (str): description
+            arg2 (3x3 numpy array): description
+
+        Returns:
+            dict: description
+
+        Raises:
+            ValueError: description
+
+        Examples:
+            description
+
+            >>> print_test ("test", "message")
+              test message
+
+        Note:
+            description
+        """
+        cs, alphas, linewidths, linestyles = \
+                get_plot_properties_from_trajectory(
+                        plot_nums=len(self._phonons))
+        bands_plot(fig=fig,
+                   phonons=self._phonons,
+                   with_dos=with_dos,
+                   mesh=mesh,
+                   band_labels=band_labels,
+                   segment_qpoints=segment_qpoints,
+                   is_auto=is_auto,
+                   xscale=xscale,
+                   npoints=npoints,
+                   cs=cs,
+                   alphas=alphas,
+                   linewidths=linewidths,
+                   linestyles=linestyles,
+                   labels=labels,
+                   )
--- twinpy/interfaces/aiida.py
+++ twinpy/interfaces/aiida.py
@@ -6,9 +6,16 @@ aiida interface
 """
 import numpy as np
 from twinpy.structure.base import get_phonopy_structure
+from twinpy.api_twinpy import get_twinpy_from_cell
+from twinpy.analysis.shear import ShearAnalizer
 from aiida.cmdline.utils.decorators import with_dbenv
 from aiida.common import NotExistentAttributeError
-from aiida.orm import load_node
+from aiida.orm import load_node, Node, QueryBuilder
+from aiida.plugins import WorkflowFactory
+from phonopy import Phonopy
+
+RELAX_WF = WorkflowFactory('vasp.relax')
+PHONOPY_WF = WorkflowFactory('phonopy.phonopy')
 
 def check_process_class(node,
                         expected_process_class:str):
@@ -42,22 +49,12 @@ def get_cell_from_aiida(aiida_structure, get_scaled_positions=True):
 class RelaxWorkChain():
     """
     relax work chain class
-
-       .. attribute:: att1
-
-          Optional comment string.
-
-
-       .. attribute:: att2
-
-          Optional comment string.
-
     """
 
     def __init__(
-           self,
-           relax_pk:int,
-       ):
+            self,
+            relax_pk:int,
+        ):
         """
         Args:
             relax_pk (int): relax pk
@@ -126,3 +123,133 @@ class RelaxWorkChain():
                 'initial_structure_pk': self._initial_structure_pk,
                 'final_structure_pk': self._final_structure_pk,
                }
+
+
+@with_dbenv()
+class PhonopyWorkChain():
+    """
+    phonon work chain class
+    """
+
+    def __init__(
+            self,
+            phonopy_pk:int,
+        ):
+        """
+        Args:
+            relax_pk (int): relax pk
+        """
+        node = load_node(phonopy_pk)
+        check_process_class(node, 'PhonopyWorkChain')
+
+        self._node = node
+        self._phonopy_pk = phonopy_pk
+        self._structure_pk = node.inputs.structure.pk
+        self._unitcell = get_cell_from_aiida(
+                load_node(self._structure_pk))
+        self._phonon_settings = node.outputs.phonon_setting_info.get_dict()
+        self._force_sets = node.outputs.force_sets.get_array('force_sets')
+
+    def get_unitcell(self):
+        """
+        get cell
+        """
+        return self._unitcell
+
+    def get_phonon(self):
+        """
+        return phonopy object
+        """
+        phonon = Phonopy(
+                get_phonopy_structure(self._unitcell),
+                supercell_matrix=self._phonon_settings['supercell_matrix'],
+                primitive_matrix=self._phonon_settings['primitive_matrix'])
+        phonon.set_displacement_dataset(
+                self._phonon_settings['displacement_dataset'])
+        phonon.set_forces(self._force_sets)
+        phonon.produce_force_constants()
+        return phonon
+
+
+@with_dbenv()
+class ShearWorkChain():
+    """
+    shear work chain class
+    """
+
+    def __init__(
+            self,
+            shear_pk:int,
+        ):
+        """
+        Args:
+            relax_pk (int): relax pk
+        """
+        node = load_node(shear_pk)
+        check_process_class(node, 'ShearWorkChain')
+
+        self._node = node
+        self._shear_pk = shear_pk
+        self._shear_conf = node.inputs.shear_conf.get_dict()
+        self._shear_ratios = \
+            node.called[-1].outputs.shear_settings.get_dict()['shear_ratios']
+        self._input_cell = get_cell_from_aiida(node.inputs.structure)
+
+    def _get_pks(self, workflow):
+        """
+        get relax and phonopy pks
+        """
+        if workflow == 'relax':
+            wf = RELAX_WF
+        elif workflow == 'phonopy':
+            wf = PHONOPY_WF
+        else:
+            raise RuntimeError("workflow must be 'relax' or 'phonopy'")
+        node_qb = QueryBuilder()
+        node_qb.append(Node, filters={'id':{'==': self._shear_pk}}, tag='wf')
+        node_qb.append(wf, with_incoming='wf', project=['id'])
+        nodes = node_qb.all()
+        node_pks = [ node[0]  for node in nodes ]
+        node_pks.reverse()
+        return node_pks
+
+    def _get_original_cells(self):
+        """
+        get original cell before standardize
+        """
+        orig_cells = []
+        for ratio in self._shear_ratios:
+            twinpy = get_twinpy_from_cell(
+                    cell=self._input_cell,
+                    twinmode=self._shear_conf['twinmode'])
+            twinpy.set_shear(
+                xshift=self._shear_conf['xshift'],
+                yshift=self._shear_conf['yshift'],
+                dim=[1,1,1],
+                shear_strain_ratio=ratio,
+                )
+            orig_cells.append(twinpy.shear.get_structure_for_export())
+        return orig_cells
+
+    def get_analyzer(self):
+        """
+        get ShearAnalizer class object
+        """
+        relax_pks = self._get_pks(workflow='relax')
+        phonon_pks = self._get_pks(workflow='phonopy')
+        input_cells = [
+            get_cell_from_aiida(load_node(relax_pk).inputs.structure)
+            for relax_pk in relax_pks ]
+        relax_cells = [
+            get_cell_from_aiida(load_node(relax_pk).outputs.relax__structure)
+            for relax_pk in relax_pks ]
+        phonons = [
+            PhonopyWorkChain(phonon_pk).get_phonon()
+            for phonon_pk in phonon_pks ]
+        analyzer = ShearAnalizer(
+            structure_type=self._shear_conf['structure_type'],
+            orig_cells=self._get_original_cells(),
+            input_cells=input_cells)
+        analyzer.set_relax_output_structures(relax_cells)
+        analyzer.set_phonons(phonons)
+        return analyzer
