5e08e5a4af192a6b2703cff15f887dd924b532e2
kei0822kei <mizokami.keiyu@gmail.com>
2020-07-23 12:54:10 +0900

    fine code hexagonal_direction

--- twinpy/lattice/hexagonal_direction.py
+++ twinpy/lattice/hexagonal_direction.py
@@ -9,35 +9,37 @@ import numpy as np
 from typing import Union
 from .lattice import Lattice
 
+
 def convert_direction_from_four_to_three(four:Union[list,
                                                     np.array,
                                                     tuple]) -> np.array:
     """
-    convert direction from four to three
+    Convert direction from four to three.
 
     Args:
         four: four indices of hexagonal direction [uvtw]
 
     Raises:
         AssertionError: len(four) != 4
-        AssertionError: u + v + t != 0
+        AssertionError: (u+v+t) != 0
 
     Returns:
         np.array: three indices
     """
     assert len(four) == 4, "the length of input list is not four"
-    u, v, t, w  = four
-    assert (u + v + t) == 0, "u+v+t is not equal to 0"
+    u, v, t, w = four
+    assert (u+v+t) == 0, "u+v+t is not equal to 0"
     U = u - t
     V = v - t
     W = w
     return np.array([U, V, W])
 
+
 def convert_direction_from_three_to_four(three:Union[list,
                                                      np.array,
                                                      tuple]) -> np.array:
     """
-    convert direction from three to four
+    Convert direction from three to four.
 
     Args:
         three: three indices of hexagonal direction [UVW]
@@ -49,7 +51,7 @@ def convert_direction_from_three_to_four(three:Union[list,
         np.array: four indices
     """
     assert len(three) == 3, "the length of input list is not three"
-    U, V, W  = three
+    U, V, W = three
     u = ( 2 * U - V ) / 3
     v = ( 2 * V - U ) / 3
     t = - ( u + v )
@@ -59,7 +61,7 @@ def convert_direction_from_three_to_four(three:Union[list,
 
 class HexagonalDirection(Lattice):
     """
-    deals with hexagonal direction
+    Deals with hexagonal direction.
     """
 
     def __init__(
@@ -67,9 +69,9 @@ class HexagonalDirection(Lattice):
            lattice:np.array,
            three:Union[list,np.array,tuple]=None,
            four:Union[list,np.array,tuple]=None,
-       ):
+           ):
         """
-        deals with hexagonal direction
+        Deals with hexagonal direction.
 
         Args:
             lattice: lattice.
@@ -77,7 +79,6 @@ class HexagonalDirection(Lattice):
             four: direction indice (four).
         """
         super().__init__(lattice=lattice)
-        # super().__init__()
         self._three = three
         self._four = four
         self.reset_indices(self._three, self._four)
@@ -94,37 +95,37 @@ class HexagonalDirection(Lattice):
                       three:Union[list,np.array,tuple]=None,
                       four:Union[list,np.array,tuple]=None):
         """
-        reset indices
+        Reset indices.
 
         Args:
             three: direction indice (three)
             four: direction indice (four)
 
         Raises:
-            ValueError: both 'three' and 'four' are None or
-                        both 'three' and 'four' are not None
+            RuntimeError: both 'three' and 'four' are None or
+                          both 'three' and 'four' are not None
         """
         if three is None and four is None:
-            raise ValueError("both 'three' and 'four' are None")
+            raise RuntimeError("both 'three' and 'four' are None")
         elif three is not None:
             four = convert_direction_from_three_to_four(three)
         elif four is not None:
             three = convert_direction_from_four_to_three(four)
         else:
-            raise ValueError("both 'three' and 'four' are not None")
+            raise RuntimeError("both 'three' and 'four' are not None")
 
         self._three = np.array(three)
         self._four = np.array(four)
 
     def inverse(self):
         """
-        set inversed plane ex. (10-12) => (-101-2)
+        Set inversed plane ex. (10-12) => (-101-2).
         """
         self.reset_indices(three=self.three*(-1))
 
     def get_cartesian(self, normalize:bool=False) -> np.array:
         """
-        get direction with the cartesian coordinate
+        Get direction with the cartesian coordinate.
 
         Args:
             normalize: if True, normalized the norm of the vector to 1
