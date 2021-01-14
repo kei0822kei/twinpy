0dc36f5824cadebbf7d005deeb911c3bf436352b
kei0822kei <mizokami.keiyu@gmail.com>
2021-01-14 20:18:40 +0900

    Add: add minimum functions for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,14 @@
-"""pytest fixtures for simplified testing."""
-from __future__ import absolute_import
+"""
+This is for pytest fixtures.
+"""
+
 import pytest
-pytest_plugins = ['aiida.manage.tests.pytest_fixtures']
 
-
-@pytest.fixture(scope='function', autouse=True)
-def clear_database_auto(clear_database):  # pylint: disable=unused-argument
-    """Automatically clear database in between tests."""
-
-
-@pytest.fixture(scope='function')
-def package_template_code(aiida_local_code_factory):
-    """Get a package_template code.
+@pytest.fixture(autouse=True, scope='function')
+def return_three():
     """
-    package_template_code = aiida_local_code_factory(
-        executable='diff', entry_point='package_template')
-    return package_template_code
+    This is sample fixture.
+    """
+    print("This is sample funciton.")
+    print("return 3")
+    return 3
