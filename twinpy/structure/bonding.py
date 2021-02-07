dd0331c19e61bbf31cc182cf49e9f41f1e14e8b5
kei0822kei <mizokami.keiyu@gmail.com>
2021-02-07 16:21:53 +0900

    Refactor: Trivial changes.

--- twinpy/structure/bonding.py
+++ twinpy/structure/bonding.py
@@ -3,6 +3,11 @@
 
 """
 Bonding base.
+
+Note:
+    Currently writen by python but it is time consuming to
+    excute the functions in this module. They are written
+    by C++ in the near future.
 """
 import numpy as np
 import itertools
