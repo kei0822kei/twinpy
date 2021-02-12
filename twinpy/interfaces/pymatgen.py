3d5129f78ded23772ddfd160c6bd163ba3e67190
kei0822kei <mizokami.keiyu@gmail.com>
2021-02-12 21:13:18 +0900

    Remove: Remove get wyckoff letter from pymatgen hexagonal structure.

--- twinpy/interfaces/pymatgen.py
+++ twinpy/interfaces/pymatgen.py
@@ -6,7 +6,6 @@ This module is the interface for pymatgen.
 """
 
 from pymatgen.core.structure import Structure
-from twinpy.properties.hexagonal import check_cell_is_hcp
 
 
 def get_pymatgen_structure(cell:tuple) -> Structure:
@@ -14,7 +13,7 @@ def get_pymatgen_structure(cell:tuple) -> Structure:
     Get pymatgen structure from cell.
 
     Args:
-        cell (tuple): cell
+        cell: Cell (lattice, scaled_positions, symbols).
     """
     return Structure(lattice=cell[0],
                      coords=cell[1],
@@ -26,33 +25,13 @@ def get_cell_from_pymatgen_structure(pmgstructure:Structure) -> tuple:
     Get cell from pymatgen.
 
     Args:
-        pmgstructure (Structure): pymatgen structure
+        pmgstructure: Pymatgen structure.
 
     Returns:
-        tuple: cell
+        tuple: Cell (lattice, scaled_positions, symbols).
     """
     lattice = pmgstructure.lattice.matrix
     scaled_positions = pmgstructure.frac_coords
     symbols = [ specie.value for specie in pmgstructure.species ]
 
     return (lattice, scaled_positions, symbols)
-
-
-def get_hexagonal_cell_wyckoff_from_pymatgen(pmgstructure:Structure) -> tuple:
-    """
-    Get hexagonal cell and wyckoff letter from pyamtgen structure.
-
-    Args:
-        pmgstructure: pymatgen structure object
-
-    Returns:
-        tuple: (lattice, scaled_positions, symbol, wyckoff)
-    """
-    lattice, scaled_positions, symbols = \
-            get_cell_from_pymatgen_structure(pmgstructure)
-    wyckoff = check_cell_is_hcp(lattice=lattice,
-                                scaled_positions=scaled_positions,
-                                symbols=symbols,
-                                get_wyckoff=True)
-
-    return (lattice, scaled_positions, symbols[0], wyckoff)
