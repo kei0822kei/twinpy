6d4b62578d1ccb16b5c6de8ab1d1c2addcbf7b16
kei0822kei <mizokami.keiyu@gmail.com>
2021-07-02 18:04:24 +0900

    Fix: Trivial change.

--- twinpy/interfaces/aiida/profile.py
+++ twinpy/interfaces/aiida/profile.py
@@ -12,14 +12,16 @@ def load_aiida_profile():
 
     try:
         from aiida import load_profile
-        from aiida.common.exceptions import ProfileConfigurationError
+    except ImportError:
+        err_msg = "Aiida is not installed. Skip loading aiida profile."
+        print(err_msg)
+        raise
+
+    from aiida.common.exceptions import ProfileConfigurationError
+    try:
         load_profile()
     except ProfileConfigurationError as err:
         err_msg = "Failed to load aiida profile. " \
                 + "Please check your aiida configuration."
         print(err_msg)
         raise ImportError from err
-    except ImportError:
-        err_msg = "Aiida is not installed. Skip loading aiida profile."
-        print(err_msg)
-        raise
